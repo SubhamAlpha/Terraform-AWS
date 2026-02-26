0323050f3b996fcfa287f977396f51164e02b634
SubhamAlpha <nanda.subham.001@gmail.com>
2026-02-26 22:12:55 +0530

    feat: add security group association for nginx server instance

--- aws-vpc-ec2-nginx/ec2.tf
+++ aws-vpc-ec2-nginx/ec2.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "nginx_server" {
    ami  = "ami-073130f74f5ffb161" # Amazon Linux 2 AMI (HVM), SSD Volume Type
   instance_type = "t3.micro"           # Updated to Free Tier eligible instance
   subnet_id     = aws_subnet.public_subnet_tf.id
+  security_groups = [aws_security_group.ngnix_sg.id]
   
   user_data = <<-EOF
               #!/bin/bash
