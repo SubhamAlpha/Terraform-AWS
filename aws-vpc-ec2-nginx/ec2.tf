52b78ef1895d077c2967a3d4bff6fc0590d136d5
SubhamAlpha <nanda.subham.001@gmail.com>
2026-02-27 23:23:35 +0530

    feat: update nginx server configuration and remove unused instance

--- aws-vpc-ec2-nginx/ec2.tf
+++ aws-vpc-ec2-nginx/ec2.tf
@@ -2,16 +2,19 @@ resource "aws_instance" "nginx_server" {
    ami  = "ami-073130f74f5ffb161" # Amazon Linux 2 AMI (HVM), SSD Volume Type
   instance_type = "t3.micro"           # Updated to Free Tier eligible instance
   subnet_id     = aws_subnet.public_subnet_tf.id
-  security_groups = [aws_security_group.ngnix_sg.id]
+  vpc_security_group_ids = [aws_security_group.ngnix_sg.id] # Associate security group
   associate_public_ip_address = true 
   
-  user_data = <<-EOF
+user_data = <<-EOF
               #!/bin/bash
-              sudo yum update -y
-              sudo amazon-linux-extras install nginx1.12 -y
-              sudo systemctl start nginx
-              sudo systemctl enable nginx
+              yum update -y
+              amazon-linux-extras enable nginx1
+              yum clean metadata
+              yum install -y nginx
+              systemctl start nginx
+              systemctl enable nginx
               EOF
+              
   tags = {
     Name = "nginx-server"
   }
