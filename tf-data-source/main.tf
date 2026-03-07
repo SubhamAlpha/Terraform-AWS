e7ffc179a064958e0451873c9b1a4858ccdf57eb
SubhamAlpha <nanda.subham.001@gmail.com>
2026-03-07 23:29:58 +0530

    feat: add AWS AMI data source and output for instance configuration

--- tf-data-source/main.tf
+++ tf-data-source/main.tf
@@ -11,3 +11,20 @@ terraform {
 provider "aws" {
   region = "eu-north-1"
 }
+
+data "aws_ami" "name" {
+  most_recent = true
+  owners = [ "amazon" ]
+}
+output "aws_ami" {
+    value = data.aws_ami.name
+}
+
+resource "aws_instance" "myserver" {
+    
+  ami  = "ami-073130f74f5ffb161" # Amazon Linux 2 AMI (HVM), SSD Volume Type
+  instance_type = "t3.micro"
+  tags = {
+    Name = "SampleServer"
+  }
+}
