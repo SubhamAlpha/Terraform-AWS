abc56ae35f2630a51f812ff30cbc0c7ea7338cd8
SubhamAlpha <nanda.subham.001@gmail.com>
2026-03-08 01:24:28 +0530

    feat: update AWS AMI reference and add security group, caller identity, and region data sources

--- tf-data-source/main.tf
+++ tf-data-source/main.tf
@@ -12,6 +12,7 @@ provider "aws" {
   region = "eu-north-1"
 }
 
+#ami
 data "aws_ami" "name" {
   most_recent = true
   owners = [ "amazon" ]
@@ -20,9 +21,33 @@ output "aws_ami" {
     value = data.aws_ami.name
 }
 
+#security group
+data "aws_security_groups" "name" {
+  tags = {
+    Name = "nginx-sg"
+  }
+}
+
+#caller identity
+data "aws_caller_identity" "name" {
+  
+}
+output "callerinfo" {
+  value = data.aws_caller_identity.name
+}
+
+#region
+data "aws_region" "name" {
+  
+}
+output "region" {
+  value = data.aws_region.name
+}
+
+
 resource "aws_instance" "myserver" {
     
-  ami  = "ami-073130f74f5ffb161" # Amazon Linux 2 AMI (HVM), SSD Volume Type
+  ami  = data.aws_ami.name.id# Amazon Linux 2 AMI (HVM), SSD Volume Type
   instance_type = "t3.micro"
   tags = {
     Name = "SampleServer"
