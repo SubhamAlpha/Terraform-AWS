b71b444b38510afb2e9a1354df0e2e8f4e3c24a6
SubhamAlpha <nanda.subham.001@gmail.com>
2026-03-08 22:27:12 +0530

    default values removed

--- tf-variables/variables.tf
+++ tf-variables/variables.tf
@@ -5,12 +5,15 @@ variable "aws_instance_type" {
       condition =  var.aws_instance_type=="t2.micro"|| var.aws_instance_type=="t3.micro"
       error_message = "instance type must be t2.micro, t3.micro"
     }
-    default     = "t3.micro"
+    
   
 }
 
 variable "volumn_size" {
     description = "what size of volumn you want to create?"
     type        = number
-    default     = 8
+    validation {
+      condition =  var.volumn_size >= 8 && var.volumn_size <= 16
+      error_message = "volumn size must be between 8 and 16"
+    }
 }
