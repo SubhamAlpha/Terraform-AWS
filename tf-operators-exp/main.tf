91353936432f16f087fce2e0cd26d6910df692a2
SubhamAlpha <nanda.subham.001@gmail.com>
2026-03-10 00:04:54 +0530

    tf-operators

--- tf-operators-exp/main.tf
+++ tf-operators-exp/main.tf
@@ -1,1 +1,34 @@
 terraform  { }
+
+#number list variable
+variable "num_list" {
+  type = list(number)
+  default = [ 1,2,3,4,5 ]
+}
+
+#list of objects variable
+variable "obj_list" {
+  type = list(object({
+    name = string
+    age  = number
+  }))
+  default = [
+    {
+      name = "Alice"
+      age  = 30
+    },
+    {
+      name = "Bob"
+      age  = 25
+    }
+  ]
+}
+
+#map variable
+variable "map_var" {
+  type = map(number)
+  default = {
+    key1 = 1
+    key2 = 2
+  }
+}
