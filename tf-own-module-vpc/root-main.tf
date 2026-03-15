976d34af3dfcc308d7428e4b3e507c5a09d0e066
SubhamAlpha <nanda.subham.001@gmail.com>
2026-03-15 22:23:18 +0530

    assigning Public or Private

--- tf-own-module-vpc/root-main.tf
+++ tf-own-module-vpc/root-main.tf
@@ -9,10 +9,12 @@ module "vpc-module" {
         public-subnet = {
             cidr_block = "10.0.0.0/24"
             az = "eu-north-1a"
+            public = true
         }
         private-subnet = {
             cidr_block = "10.0.1.0/24"
             az = "eu-north-1b"
+            public = false
         }
     }     
 }
